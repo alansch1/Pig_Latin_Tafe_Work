063609fb2768ed4547f74f4a93e8f834d6d7bc08
Alan Schiffler - Tafe <alan_sch1@hotmail.com>
2014-07-29 11:37:40 +1000

    Moved Files To Github
    File Relocation

---
+++ PigLatinTranslator/Schiffler_Alan_OPP2AnotherLang_AS1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PigLatinTranslator
+{
+	partial class frmPigLatin
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblText = new System.Windows.Forms.Label();
+			this.lblText2 = new System.Windows.Forms.Label();
+			this.txtEnglish = new System.Windows.Forms.TextBox();
+			this.txtLatin = new System.Windows.Forms.TextBox();
+			this.btnTranslate = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblText
+			// 
+			this.lblText.AutoSize = true;
+			this.lblText.Location = new System.Drawing.Point(45, 19);
+			this.lblText.Name = "lblText";
+			this.lblText.Size = new System.Drawing.Size(122, 13);
+			this.lblText.TabIndex = 0;
+			this.lblText.Text = "Enter English Text Here:";
+			// 
+			// lblText2
+			// 
+			this.lblText2.AutoSize = true;
+			this.lblText2.Location = new System.Drawing.Point(45, 142);
+			this.lblText2.Name = "lblText2";
+			this.lblText2.Size = new System.Drawing.Size(106, 13);
+			this.lblText2.TabIndex = 1;
+			this.lblText2.Text = "Pig Latin Translation:";
+			// 
+			// txtEnglish
+			// 
+			this.txtEnglish.Location = new System.Drawing.Point(48, 44);
+			this.txtEnglish.Multiline = true;
+			this.txtEnglish.Name = "txtEnglish";
+			this.txtEnglish.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtEnglish.Size = new System.Drawing.Size(244, 84);
+			this.txtEnglish.TabIndex = 1;
+			this.txtEnglish.TextChanged += new System.EventHandler(this.txtEnglish_TextChanged);
+			// 
+			// txtLatin
+			// 
+			this.txtLatin.Location = new System.Drawing.Point(48, 175);
+			this.txtLatin.Multiline = true;
+			this.txtLatin.Name = "txtLatin";
+			this.txtLatin.ReadOnly = true;
+			this.txtLatin.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtLatin.Size = new System.Drawing.Size(244, 84);
+			this.txtLatin.TabIndex = 2;
+			// 
+			// btnTranslate
+			// 
+			this.btnTranslate.Location = new System.Drawing.Point(48, 282);
+			this.btnTranslate.Name = "btnTranslate";
+			this.btnTranslate.Size = new System.Drawing.Size(75, 23);
+			this.btnTranslate.TabIndex = 3;
+			this.btnTranslate.Text = "&Translate";
+			this.btnTranslate.UseVisualStyleBackColor = true;
+			this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+			// 
+			// btnClear
+			// 
+			this.btnClear.Location = new System.Drawing.Point(141, 282);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 23);
+			this.btnClear.TabIndex = 4;
+			this.btnClear.Text = "&Clear";
+			this.btnClear.UseVisualStyleBackColor = true;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnExit.Location = new System.Drawing.Point(290, 282);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 5;
+			this.btnExit.Text = "E&xit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// frmPigLatin
+			// 
+			this.AcceptButton = this.btnTranslate;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnExit;
+			this.ClientSize = new System.Drawing.Size(384, 309);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnTranslate);
+			this.Controls.Add(this.txtLatin);
+			this.Controls.Add(this.txtEnglish);
+			this.Controls.Add(this.lblText2);
+			this.Controls.Add(this.lblText);
+			this.Name = "frmPigLatin";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Pig Latin Translator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label lblText;
+		private System.Windows.Forms.Label lblText2;
+		private System.Windows.Forms.TextBox txtEnglish;
+		private System.Windows.Forms.TextBox txtLatin;
+		private System.Windows.Forms.Button btnTranslate;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Button btnExit;
+	}
+}
+
---
+++ PigLatinTranslator/Schiffler_Alan_OPP2AnotherLang_AS1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PigLatinTranslator
+{
+	public partial class frmPigLatin : Form
+	{
+		// -------------------------------------------------------------------------------------------------------------------------------//
+		
+		private static String vowelChars = "AEIOUaeiou";
+		private static String vowelChars2 = "AEIOUYaeiouy";
+		private static String specChars = "@ # $ % ^ & * - _ = + ` ~ | \\ < > , / 1 2 3 4 5 6 7 8 9 0 ! . ?";
+		private static String[] specialCharacters = specChars.Split(' ');
+
+		// -------------------------------------------------------------------------------------------------------------------------------//
+		public frmPigLatin()
+		{
+			InitializeComponent();
+			txtEnglish.Focus();
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			txtEnglish.Text = "";
+			txtLatin.Text = "";
+			txtEnglish.Focus();
+		}
+
+		private void btnTranslate_Click(object sender, EventArgs e)
+		{
+			if (String.IsNullOrEmpty(txtEnglish.Text))
+			{
+				MessageBox.Show("Please Enter A Word For The Program To Work!!");
+			}
+			else if (String.IsNullOrWhiteSpace(txtEnglish.Text))
+			{
+				MessageBox.Show("Please Enter A Word For The Program To Work!! (Not Just Blank Spaces!!)");
+			}
+			else
+			{
+				TextTranslate();
+			}
+		}
+
+		private void txtEnglish_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void TextTranslate()
+		{
+			String phrase = txtEnglish.Text.Trim();									// Clean-up the start and finish spaces.
+			String[] wordsArray = phrase.Split(' ');								// Split words into the 'words' array.
+			StringBuilder sb = new StringBuilder();
+
+			foreach (String word in wordsArray)
+			{
+				String myWord = word;
+				String firstLetter = myWord.Substring(0, 1);
+				String lastCharacter = null;
+
+				if (endPunct(myWord) == true)										// If there is punctuation at the end of the word, word is reassembled and put back together keeping the punctuation at the end
+				{
+					lastCharacter = myWord.Substring(myWord.Length - 1, 1);
+					myWord = myWord.Remove(myWord.Length - 1, 1);
+				}
+
+				if (!myWord.Equals("") && !specialCharacters.Any(myWord.Contains))	// If the word is not blank - then do
+				{
+																					// Grab the first letter of each word
+
+					if (vowelChars.Contains(firstLetter))							//	to the end of the word.
+					{
+						if (word.Equals(word.ToUpper()))
+						{															// Checks if the word is all caps, if so makes way caps.
+							myWord += "WAY";
+						}
+						else
+						{
+							myWord += "way";
+						}
+					}
+					else															 // If word does not start with a vowel, remove first letter
+					{                                                                // and move it to the end of the word + 'ay'
+						if (!vowelChars.Contains(firstLetter))
+						{
+							if (word.Equals(word.ToUpper()))						 // Checks if the word is all caps, if so makes ay caps, and moves letter
+							{
+								myWord = createConsonant(myWord).ToUpper();
+							}
+							else if (firstLetter.Equals(firstLetter.ToUpper()))      //Checks to see if the first letter is a capital
+							{
+								myWord = createConsonant(myWord);
+								myWord = myWord.Substring(0, 1).ToUpper() + myWord.Substring(1, myWord.Length - 1).ToLower();
+							}
+							else
+							{
+								myWord = createConsonant(myWord).ToLower();
+							}
+						}
+					}
+				}
+				else
+				{
+					txtLatin.Text = "Please Enter A Word";
+				}
+				sb.Append(myWord + lastCharacter + " ");							// Adds all strings to stringbuilder with spaces between words
+				txtLatin.Text = sb.ToString().TrimEnd(' ');							// Prints Pig Latin to screen
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+		}
+
+		// Getting the Index of the first Vowel & the letter y (where applicable) ------------------------------------//
+		private static int GetVowelIndex(String word)
+		{
+			int index = 0;
+			if (word.ToLower().Substring(1, word.Length - 1).Contains('y'))
+			{
+				for (int i = 0; i < word.Length; i++)
+				{
+					if (vowelChars2.Contains(word[i]))
+					{
+						index = i;
+						break;
+					}
+				}
+				return index;
+			}
+			else
+			{
+				for (int i = 0; i < word.Length; i++)
+				{
+					if (vowelChars.Contains(word[i]))
+					{
+						index = i;
+						break;
+					}
+				}
+				return index;
+			}
+		}
+
+		// Method for word with consonant -------------------------------------------------------------------------------//
+		private static String createConsonant(String word)
+		{
+			int vowelIndex = GetVowelIndex(word);
+			String conWord = word.Substring(vowelIndex) + word.Substring(0, vowelIndex) + "ay";
+			return conWord;
+		}
+
+		// Determine if word has punctuation on end ---------------------------------------------------------------------//
+		private static bool endPunct(String word)
+		{
+			Char lastChar = System.Convert.ToChar(word.Substring(word.Length - 1, 1));
+
+			if (Char.IsPunctuation(lastChar) == true)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
